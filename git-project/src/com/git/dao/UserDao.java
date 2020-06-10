f38b699963e54dfafd0fa3066ee9600fbc3284bc
TanSimin2020 <1307845399@qq.com>
2020-06-10 11:01:13 +0800

    对用户模块的DDL操作

---
+++ git-project/src/com/git/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.git.dao;
+
+import java.util.List;
+
+public class UserDao {
+
+	public void addUser(int id,String name,String password) {
+		//增加操作
+	}
+	
+	public void deleteUser(int id) {
+		//删除操作
+	}
+	
+	public void updateUser(int id,String newNames,String newPasswords) {
+		//修改操作
+	}
+	
+	public List<Object> addUser(int pageIndex,int pageSize) {
+		//查找操作
+		return null;
+	}
+}
