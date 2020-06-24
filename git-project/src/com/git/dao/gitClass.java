ebef86a9fbd8f5f98092bf5cefa9f988946f8e16
TanSimin2020 <1307845399@qq.com>
2020-06-24 11:01:53 +0800

    add file

---
+++ git-project/src/com/git/dao/gitClass.java
@@ -0,0 +1,19 @@
+package com.git.dao;
+
+public class gitClass {
+
+	private int id;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
